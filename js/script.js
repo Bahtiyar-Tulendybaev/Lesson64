1bdcbf6c43836d849a20284bb0c75dc0cdf1a253
Bahtiyar Tulendybaev <bahtiyar_tulendybaev@mail.ru>
2023-04-28 10:25:04 +0600

    add script.js

---
+++ js/script.js
@@ -0,0 +1,38 @@
+const list = document.querySelector('ul');
+list.addEventListener('dblclick', function (ev) {
+
+    if(ev.target.tagName === "LI") {
+        ev.target.classList.toggle('checked');
+        toLocal();
+
+    } else if(ev.target.tagName === "SPAN") {
+        const div = ev.target.parentNode;
+
+        div.remove();
+        toLocal()
+
+    }
+}, false);
+
+function newElement() {
+    const li = document.createElement('li');
+    const inputValue = document.getElementById('task').value;
+
+    const text = document.createTextNode(inputValue);
+    li.appendChild(text);
+
+    if(inputValue === "") {
+        alert("Введите вашу задачу!");
+    } else {
+        document.getElementById('list').appendChild(li);
+    }
+
+    document.getElementById('task').value = "";
+    toLocal();
+}
+let tasks;
+
+function toLocal() {
+    tasks = list.innerHTML;
+    localStorage.setItem('tasks', tasks);
+}
