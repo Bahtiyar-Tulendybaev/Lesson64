403bf25144cfd0b0faa7eb4e93a48f984338c8ac
Abdilova Kamila <kamilaabdilova14@gmail.com>
2023-04-30 22:30:34 +0600

    added HTML and CCS

--- js/script.js
+++ js/script.js
@@ -1,37 +1,69 @@
-const list = document.querySelector('ul');
-list.addEventListener('dblclick', function (ev) {
-    if(ev.target.tagName === "LI") {
-        ev.target.classList.toggle('checked');
-        toLocal();
+// const list = document.querySelector('ul');
+// list.addEventListener('dblclick', function (ev) {
+//     if(ev.target.tagName === "LI") {
+//         ev.target.classList.toggle('checked');
+//         toLocal();
+//
+//     } else if(ev.target.tagName === "SPAN") {
+//         const div = ev.target.parentNode;
+//
+//         div.remove();
+//         toLocal()
+//
+//     }
+// }, false);
+//
+// function newElement() {
+//     const li = document.createElement('li');
+//     const inputValue = document.getElementById('task').value;
+//
+//     const text = document.createTextNode(inputValue);
+//     li.appendChild(text);
+//
+//     if(inputValue === "") {
+//         alert("Введите вашу задачу!");
+//     } else {
+//         document.getElementById('list').appendChild(li);
+//     }
+//
+//     document.getElementById('task').value = "";
+//     toLocal();
+// }
+// let tasks;
+//
+// function toLocal() {
+//     tasks = list.innerHTML;
+//     localStorage.setItem('tasks', tasks);
+// }
 
-    } else if(ev.target.tagName === "SPAN") {
-        const div = ev.target.parentNode;
 
-        div.remove();
-        toLocal()
 
+
+
+
+const form = document.querySelector('#task-form');
+const taskList = document.querySelector('#task-list');
+
+form.addEventListener('submit', function(e) {
+    e.preventDefault();
+
+
+    const taskInput = document.querySelector('#task-input');
+    const inputValue = taskInput.value;
+
+
+    if(inputValue.trim() !== '') {
+        const newTask = document.createElement('li');
+        newTask.textContent = inputValue;
+        taskList.appendChild(newTask);
+
+
+        taskInput.value = '';
+        taskInput.focus();
+
+
+        newTask.addEventListener('dblclick', function() {
+            newTask.classList.toggle('checked');
+        });
     }
-}, false);
-
-function newElement() {
-    const li = document.createElement('li');
-    const inputValue = document.getElementById('task').value;
-
-    const text = document.createTextNode(inputValue);
-    li.appendChild(text);
-
-    if(inputValue === "") {
-        alert("Введите вашу задачу!");
-    } else {
-        document.getElementById('list').appendChild(li);
-    }
-
-    document.getElementById('task').value = "";
-    toLocal();
-}
-let tasks;
-
-function toLocal() {
-    tasks = list.innerHTML;
-    localStorage.setItem('tasks', tasks);
-}
+});
